a09e9686f580fd51aafccb9f0e1b3f383e4f514a
Aadithya Niranjan <aadinir@amazon.com>
2026-04-13 20:40:49 +0530

    minor fixes

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,47 @@
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fs;
 use std::io;
+use serde::{Serialize, Deserialize};
+use std::fs;
 use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
-struct SqueezeStore {
-    store: HashMap<String, String>,
+struct SqueezeStore{
+        store: HashMap<String, String>,
 }
 impl SqueezeStore {
-    fn new() -> Self {
-        SqueezeStore {
-            store: HashMap::new(),
+        fn new() -> Self {
+            SqueezeStore {
+                store: HashMap::new(),
+            }
         }
-    }
 
-    fn get(&self, key: &str) -> Option<&str> {
-        self.store.get(key).map(|s| s.as_str())
-    }
-    fn set(&mut self, key: String, value: String) {
-        self.store.insert(key, value);
-    }
-    fn load_from_file(path: &Path) -> io::Result<Self> {
-        if !path.exists() {
-            return Ok(SqueezeStore::new());
+        fn get(&self, key:&str)-> Option<&str> {
+            self.store.get(key).map(|s| s.as_str())
         }
-        let contents = fs::read_to_string(path)?;
-        let loaded: SqueezeStore = serde_json::from_str(&contents)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        fn set(&mut self, key: String, value: String) {
+            self.store.insert(key, value);
+        }
+        fn load_from_file(path: &Path) -> io::Result<Self> {
+            if !path.exists(){
+                return Ok(SqueezeStore::new());
+            }
+            let contents = fs::read_to_string(path)?;
+            let loaded:SqueezeStore= serde_json::from_str(&contents)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            
+            Ok(loaded)
 
-        Ok(loaded)
-    }
+        }
 
-    fn save_to_file(&self, path: &Path) -> io::Result<()> {
-        let contents = serde_json::to_string(&self)
-            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-        fs::write(path, contents)
-    }
+        fn save_to_file(&self, path: &Path) -> io::Result<()> {
+            let contents = serde_json::to_string(&self)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            fs::write(path, contents)
+        }
+
 }
 
-enum Command {
+enum Command{
     Set(String, String),
     Get(String),
     Exit,
@@ -48,9 +50,9 @@ enum Command {
 fn parse_command(input: &str) -> Result<Command, String> {
     let mut parts = input.splitn(3, ' ');
 
-    let command = match parts.next().ok_or("Empty command") {
+    let command = match parts.next().ok_or("Empty command"){
         Ok(val) => val,
-        Err(err) => return Err(err.to_string()),
+        Err(err) => return Err(err.to_string())
     };
 
     match command.to_lowercase().as_str() {
@@ -72,33 +74,34 @@ fn parse_command(input: &str) -> Result<Command, String> {
 }
 
 fn main() {
-    let mut db = match SqueezeStore::load_from_file(Path::new("store.json")) {
-        Ok(val) => val,
-        Err(err) => {
-            eprintln!("Failed to load store: {err}");
-            return;
-        }
+   
+    let mut db = match SqueezeStore::load_from_file(Path::new("store.json")){
+        Ok(val)=> val,
+        Err(err)=>{                                                                                                                                                                
+            eprintln!("Failed to load store: {err}");                                                                                                                                
+            return;                                                                                                                                                                  
+        }              
     };
-
+  
     println!("welcome to SqueezeStore! A simple in-memory key-value store.");
 
-    loop {
+    loop{
         println!("Enter a command (set/get/exit):");
         let mut command = String::new();
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read line");
-        match parse_command(command.trim()) {
+        io::stdin().read_line(&mut command).expect("Failed to read line");
+        match parse_command(command.trim()){
             Ok(Command::Set(key, value)) => {
                 db.set(key, value);
                 match db.save_to_file(Path::new("store.json")) {
                     Ok(()) => println!("Key-value pair set successfully."),
                     Err(e) => eprintln!("Error saving to file: {e}"),
                 }
-            }
-            Ok(Command::Get(key)) => match db.get(&key) {
-                Some(val) => println!("Value for '{key}': {val}"),
-                None => println!("Key '{key}' not found"),
+            },
+            Ok(Command::Get(key)) => {
+                match db.get(&key){
+                    Some(val) => println!("Value for '{}': {}", key, val),
+                    None => println!("Key '{}' not found", key),
+                }
             },
             Ok(Command::Exit) => {
                 match db.save_to_file(Path::new("store.json")) {
@@ -106,8 +109,10 @@ fn main() {
                     Err(e) => eprintln!("Error saving to file: {e}"),
                 }
                 break;
-            }
-            Err(e) => println!("Error: {e}"),
+            },
+            Err(e) => println!("Error: {}", e),
         }
+
+
     }
 }
